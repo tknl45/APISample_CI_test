a18cbf91f25d26a85c06e658ae6bfdff7c8f3e77
unknown <five@DESKTOP-NL2NTAS.cdsafi.com>
2018-07-17 08:21:39 +0800

    first commit

---
+++ app/Controllers/DemoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace APISample.Controllers
+{
+    [Route("api/[controller]")]
+    public class DemoController : BaseController
+    {
+
+        [HttpGet("demo_message")]
+        public void demo_message(string message)
+        {
+            this.Message = message;
+        }
+
+        [HttpPost("demo_data")]
+        public void demo_data(string message, [FromBody]Object pushForm)
+        {
+            this.Message = message;
+            this.Data = pushForm;
+        }
+
+        [HttpGet("demo_error")]
+        public void demo_error(string message)
+        {
+            throw new ArgumentException("ERROR");
+        }
+
+
+
+    }
+
+
+    
+}
---
+++ app/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Swashbuckle.AspNetCore.Swagger;
+
+using APISample.Filters;
+using APISample.Middleware;
+
+namespace APISample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //加入cross domain
+            services.AddCors();
+
+            //加入過濾器
+            services.AddMvc(config =>
+                {
+                    config.Filters.Add(typeof (ExceptionFilter));
+                    config.Filters.Add<ResultFilter>();
+                }
+            );
+
+            //loggin
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddFile("app.log", append: true);
+            });
+
+
+
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "APISample API",
+                    Description = "APISample - ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "SHIH-WU CHENG",
+                        Email = string.Empty
+                    }
+                });
+            });           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                // 錯誤處理
+                app.UseErrorHandling();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PushProxy API V1");
+            });
+
+
+            app.UseMvc();
+
+            //使用wwwroot靜態檔案
+            app.UseStaticFiles();
+
+            //預設文件
+            app.UseDefaultFiles();
+
+            app.UseFileServer();
+        }
+    }
+}
